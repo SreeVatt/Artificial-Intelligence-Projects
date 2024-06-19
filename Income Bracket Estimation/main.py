0007629a4354a3a7b441c5ac4a7860f347503af4
SreeVathsan <91870603+SreeVatt@users.noreply.github.com>
2024-06-19 22:59:27 +0530

    Add files via upload

--- Income Bracket Estimation/main.py
+++ Income Bracket Estimation/main.py
@@ -4,55 +4,56 @@ Created on Wed Jun  5 22:29:07 2024
 
 @author: sreev
 """
+import numpy as np
 from sklearn import preprocessing
 from sklearn.naive_bayes import GaussianNB
-import numpy as np
+from sklearn.model_selection import train_test_split, cross_val_score
 
-input_file='.\\Data\\adult.data'
-#Read The Data
-x=[]
-y=[]
-count_lessthan50k=0
-count_morethan50k=0
-num_images_threshold=1000
+input_file = '.\\Data\\adult.data'
+x = []
+y = []
+count_lessthan50k = 0
+count_morethan50k = 0
+num_images_threshold = 1000
 
-with open(input_file,'r') as f:
-    for line in f.reaflines():
+with open(input_file, 'r') as f:
+    for line in f.readlines():
         if '?' in line:
             continue
-        data=line[:1].split(',')
-        if data[-1]=='<=50K' and count_lessthan50k<num_images_threshold:
+        data = line.strip().split(', ')  # Adjusted to handle spaces after commas if any
+        if data[-1] == '<=50K' and count_lessthan50k < num_images_threshold:
             x.append(data)
-            count_lessthan50k=count_lessthan50k+1
-        elif data[-1]=='>50K' and count_morethan50k<num_images_threshold:
-            x.append(data)
-        if count_lessthan50k>=num_images_threshold and count_morethan50k>= num_images_threshold:
+            count_lessthan50k += 1
+        elif data[-1] == '>50K' and count_morethan50k < num_images_threshold:
+            y.append(data)  # Append to y for '>50K' case
+            count_morethan50k += 1
+        if count_lessthan50k >= num_images_threshold and count_morethan50k >= num_images_threshold:
             break
-x=np.array(x)
 
-#Convert string data into numerical data
-label_encoder=[]
-x_encoded=np.empty(x.shape)
-for i,item in enumerate(x[0]):
+# Convert lists to NumPy arrays
+x = np.array(x)
+y = np.array(y)
+
+# Encode categorical data
+label_encoders = []
+x_encoded = np.empty(x.shape)
+for i, item in enumerate(x[0]):
     if item.isdigit():
-        x_encoded[:,i]=x[:,i]
+        x_encoded[:, i] = x[:, i].astype(float)  # Convert to float if numeric
     else:
-        label_encoder.append(preprocessing.LabelEncoder())
-        x_encoded[:,i]=label_encoder[-1].fit_transform(x[:,i])
-x=x_encoded[:,:-1].astype(int)
-y=x_encoded[:,-1].astype(int)
-    
-#Building Classifier
-classifier_gaussiannb=GaussianNB()
-classifier_gaussiannb.fit(x,y)
+        label_encoders.append(preprocessing.LabelEncoder())
+        x_encoded[:, i] = label_encoders[-1].fit_transform(x[:, i])
 
-#Cross Validation
-from sklearn.model_selection import cross_validate,cross_val_score
-X_train, X_test, y_train, y_test =cross_validate.train_test_split(x, y, test_size=0.25,random_state=5)
+x = x_encoded[:, :-1].astype(int)
+y = x_encoded[:, -1].astype(int)
+
+# Split data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.25, random_state=5)
+
+# Build and train classifier
 classifier_gaussiannb = GaussianNB()
 classifier_gaussiannb.fit(X_train, y_train)
-y_test_pred = classifier_gaussiannb.predict(X_test)
 
-# compute F1 score of the classifier
-f1 = cross_val_score(classifier_gaussiannb,x, y, scoring='f1_weighted', cv=5)
-print("F1 score: " + str(round(100*f1.mean(), 2)) + "%")
+# Evaluate classifier using cross-validation
+f1_scores = cross_val_score(classifier_gaussiannb, x, y, scoring='f1_weighted', cv=5)
+print("F1 score: {:.2f}%".format(100 * f1_scores.mean()))
